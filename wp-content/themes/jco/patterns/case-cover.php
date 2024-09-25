82bc7304c025862ca639ed6c14b37c48a82a5813
Lotta Hardman <lotta@jco.fi>
2024-09-25 14:35:49 +0300

    feat(patterns): case pattern

---
+++ wp-content/themes/jco/patterns/case-cover.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Case Cover
+ * Slug: jcore/case-cover
+ * Description:
+ * Categories: header
+ * Keywords: case, hero, case hero
+ * Viewport Width: 1280
+ * Block Types:
+ * Post Types: case
+ * Inserter: true
+ */
+?>
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"overlayColor":"white","isUserOverlayColor":true,"focalPoint":{"x":0.53,"y":0.35},"minHeight":90,"minHeightUnit":"vh","isDark":false,"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull is-light" style="min-height:90vh"><span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-bottom:0"><!-- wp:post-title {"level":1,"align":"wide","className":"case-heading","fontSize":"maxi"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"left","className":"case-words","fontSize":"paragraph","fontFamily":"molitor"} -->
+<p class="has-text-align-left case-words has-molitor-font-family has-paragraph-font-size"><strong>ART DIRECTION, BRANDING, DIGITAL STRATEGY, UX, VISUAL DESIGN</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
