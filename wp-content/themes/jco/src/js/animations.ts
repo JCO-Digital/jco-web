d23bda7f68a6a49b1dd71b214e1cda153c45156a
Maxemilian Grönblom <maxemiliang@gmail.com>
2024-09-25 18:50:56 +0300

    feat: improved the animations ✨

--- wp-content/themes/jco/src/js/animations.ts
+++ wp-content/themes/jco/src/js/animations.ts
@@ -9,14 +9,14 @@ window.addEventListener("DOMContentLoaded", () => {
 function animateFloating(el: HTMLElement) {
   const tl = gsap.timeline({ repeat: 0 });
   const animationEl = el.querySelector("img");
-
-  floatElement(tl, animationEl, 10);
+  floatElement(tl, animationEl, 10, el.classList.contains("scale-separately"));
 
   el.addEventListener("mouseenter", () => {
     tl.pause();
     gsap.to(animationEl, {
       scale: 1.1,
       rotation: 0,
+      skew: 0,
       x: 0,
       y: 0,
       duration: 0.5,
@@ -27,6 +27,7 @@ function animateFloating(el: HTMLElement) {
     gsap.to(animationEl, {
       scale: 1,
       rotation: 0,
+      skew: 0,
       x: 0,
       y: 0,
       duration: 0.5,
@@ -38,23 +39,46 @@ function animateFloating(el: HTMLElement) {
   });
 }
 
-function floatElement(tl: gsap.core.Timeline, el: HTMLElement, radius: number) {
+function floatElement(
+  tl: gsap.core.Timeline,
+  el: HTMLElement,
+  radius: number,
+  scaleSeparately: boolean = false,
+) {
   // Randomize the position of the element within the radius
-  const x = Math.random() * radius * 2 - radius;
-  const y = Math.random() * radius * 2 - radius;
-  // Randomize the scale and rotation of the element within the range of 0.01 to 0.99 (for scale) and -5 to 5 (for rotation)
-  const scale = Math.random() * 0.01 + 0.99;
-  const rotation = Math.random() * 10 - 5;
+  const x = getRandomBetween(-radius, radius);
+  const y = getRandomBetween(-radius, radius);
+  // Randomize the scale and rotation of the element within the range of 0.97 and 1.04 (for a scale) and -5 to 5
+  // (for rotation)
+  const scale = getRandomBetween(0.97, 1.04);
+  let scaleX = scale;
+  let scaleY = scale;
+  if (scaleSeparately) {
+    scaleX = getRandomBetween(0.97, 1.04);
+    scaleY = getRandomBetween(0.97, 1.04);
+  }
+  const rotation = getRandomBetween(-5, 5);
+
+  // Skew very slightly 0.02 to 0.2
+  const skewX = getRandomBetween(0.02, 0.2);
+  const skewY = getRandomBetween(0.02, 0.2);
 
   tl.to(el, {
     x,
     y,
-    scale,
+    scaleX,
+    scaleY,
     rotation,
-    duration: Math.random() * 2 + 1,
+    skewX,
+    skewY,
+    duration: getRandomBetween(1.75, 3),
     ease: "none",
     onComplete: () => {
-      floatElement(tl, el, radius);
+      floatElement(tl, el, radius, scaleSeparately);
     },
   });
+}
+
+function getRandomBetween(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
 }
