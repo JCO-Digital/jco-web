73044ac348a177db76f643cbf8fb7778b7b94af9
Lotta Hardman <lotta@jco.fi>
2024-09-25 16:32:14 +0300

    feat(pattern): three col grid

---
+++ wp-content/themes/jco/patterns/three-column-grid.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Title: Three column Grid
+ * Slug: jcore/three-column-grid
+ * Description:
+ * Categories: text
+ * Keywords: three col, three column grid, grid, columns
+ * Viewport Width: 1280
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"metadata":{"categories":["text"],"patternName":"jcore/ilme/twothirdcolumns","name":"Two Third Columns"},"align":"wide","style":{"dimensions":{"minHeight":"0px"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"90px"}} -->
+<div class="wp-block-group alignwide" style="min-height:0px"><!-- wp:group {"style":{"layout":{"columnSpan":4,"rowSpan":1},"spacing":{"padding":{"bottom":"0"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group" style="padding-bottom:0"><!-- wp:image {"id":4387,"sizeSlug":"large","linkDestination":"none","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/Jcore-logo_color-slogan-blck-1024x674.png" alt="JCORE" class="wp-image-4387"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"layout":{"columnSpan":4,"rowSpan":1},"spacing":{"padding":{"top":"0","bottom":"0"}}},"fontSize":"heading-3"} -->
+<h3 class="wp-block-heading has-text-align-center has-heading-3-font-size" style="padding-top:0;padding-bottom:0"><strong>ATTRACT</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Sivuston haluttiin myös herättävän huomiota kuvien ja niiden esitystavan kautta.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"layout":{"columnSpan":4,"rowSpan":1},"spacing":{"padding":{"bottom":"0"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group" style="padding-bottom:0"><!-- wp:spacer {"className":"hide"} -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer hide"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":4364,"sizeSlug":"large","linkDestination":"none","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/Jco-welovegames-1024x863.webp" alt="" class="wp-image-4364"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"layout":{"columnSpan":4,"rowSpan":1},"spacing":{"padding":{"top":"0","bottom":"0"}}},"fontSize":"heading-3"} -->
+<h3 class="wp-block-heading has-text-align-center has-heading-3-font-size" style="padding-top:0;padding-bottom:0"><strong>INTERACT</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Sivuston haluttiin myös herättävän huomiota kuvien ja niiden esitystavan kautta.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"layout":{"rowSpan":1,"columnSpan":"4"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":4387,"sizeSlug":"large","linkDestination":"none","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/Jcore-logo_color-slogan-blck-1024x674.png" alt="JCORE" class="wp-image-4387"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"heading-3"} -->
+<h3 class="wp-block-heading has-text-align-center has-heading-3-font-size"><strong>AMAZE</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Sivuston haluttiin myös herättävän huomiota kuvien ja niiden esitystavan kautta.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
