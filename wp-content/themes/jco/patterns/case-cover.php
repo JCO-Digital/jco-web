743ec0e477eddb925f7101af51e288de36d7d90b
Lotta Hardman <lotta@jco.fi>
2024-09-30 13:25:22 +0300

    feat(langs, patterns, block): mo-po fi/sv, case patterm, some block

--- wp-content/themes/jco/patterns/case-cover.php
+++ wp-content/themes/jco/patterns/case-cover.php
@@ -2,11 +2,11 @@
 /**
  * Title: Case Cover
  * Slug: jcore/case-cover
- * Description:
+ * Description: 
  * Categories: header
  * Keywords: case, hero, case hero
  * Viewport Width: 1280
- * Block Types:
+ * Block Types: 
  * Post Types: case
  * Inserter: true
  */
@@ -16,8 +16,8 @@
 <div class="wp-block-group alignwide" style="padding-bottom:0"><!-- wp:post-title {"level":1,"align":"wide","className":"case-heading","fontSize":"maxi"} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide"><!-- wp:paragraph {"align":"left","className":"case-words","fontSize":"paragraph","fontFamily":"molitor"} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0"><!-- wp:paragraph {"align":"left","className":"case-words","fontSize":"paragraph","fontFamily":"molitor"} -->
 <p class="has-text-align-left case-words has-molitor-font-family has-paragraph-font-size"><strong>ART DIRECTION, BRANDING, DIGITAL STRATEGY, UX, VISUAL DESIGN</strong></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div></div>
