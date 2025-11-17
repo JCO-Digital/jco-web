c1089280df3380bf90d03272746ce231972b3311
Linus <liinus.wiberg@gmail.com>
2025-11-17 13:20:31 +0200

    Chore: jcore update + mainWP ci

--- wp-content/mu-plugins/debug.php
+++ wp-content/mu-plugins/debug.php
@@ -4,4 +4,6 @@ Plugin Name: JCOre Debug level handler.
 Description: Set debug level to show.
 */
 
-error_reporting(E_ALL );
+error_reporting(
+    E_ALL & ~E_NOTICE & ~E_USER_NOTICE & ~E_DEPRECATED & ~E_USER_DEPRECATED,
+);
